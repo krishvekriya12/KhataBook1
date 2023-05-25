f7731ded3766596d4e25d8c2f6ce317b2c22ba5e
BoyWithNoHeart007 <ravibodara73@gmail.com>
2023-05-25 11:16:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/khatabook/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.khatabook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.khatabook.Fragments.AddFragment
+import com.example.khatabook.Fragments.TransactionFragment
+import com.example.khatabook.databinding.ActivityMainBinding
+import me.ibrahimsn.lib.OnItemSelectedListener
+import me.ibrahimsn.lib.SmoothBottomBar
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomBar.onItemSelectedListener = object : OnItemSelectedListener{
+
+            override fun onItemSelect(pos: Int): Boolean {
+
+                when(pos) {
+                    0->{
+
+                    }
+                    1->{
+                        loadFragment(AddFragment())
+                    }
+                    2->{
+
+                        loadFragment(TransactionFragment())
+                    }
+
+                }
+
+                return false
+            }
+
+
+        }
+
+
+    }
+
+    private fun loadFragment(fragment : Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.FrameFragment, fragment)
+            .commit()
+    }
+}
